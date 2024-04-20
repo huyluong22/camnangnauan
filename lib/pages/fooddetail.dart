d9465c26a7a673179262710ce709bbe12a3d85ca
huyluong22 <huyluong22@gmail.com>
2024-04-20 22:37:58 +0700

    them notes cho mon an

--- lib/pages/fooddetail.dart
+++ lib/pages/fooddetail.dart
@@ -1,9 +1,12 @@
 import 'package:camnangnauan/pages/favoritepage.dart';
 import 'package:camnangnauan/pages/homepage.dart';
 import 'package:camnangnauan/screen/signin_screen.dart.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageDetail extends StatefulWidget {
+  final String documentId;
   final String anh;
   final String huongDan;
   final String moTa;
@@ -11,7 +14,7 @@ class FoodPageDetail extends StatefulWidget {
   final String ten;
   final String thoiGian;
   final String typeMon;
-  final String UserNotes;
+  final List<dynamic> UserNotes;
   final List<dynamic> UsersLike;
 
   const FoodPageDetail({
@@ -24,13 +27,110 @@ class FoodPageDetail extends StatefulWidget {
     required this.typeMon,
     required this.UserNotes,
     required this.UsersLike,
+    required this.documentId,
   });
 
   @override
   _FoodPageDetailState createState() => _FoodPageDetailState();
 }
 
+
 class _FoodPageDetailState extends State<FoodPageDetail> {
+  final _notesController = TextEditingController();
+  void _showNotesDialog() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final currentUserUID = user.uid;
+
+      // Kiểm tra xem đã có notes của người dùng hiện tại chưa
+      final DocumentSnapshot document = await FirebaseFirestore.instance.collection('food').doc(widget.documentId).get();
+      final List<dynamic> userNotes = document['UserNotes'] ?? [];
+
+      String initialNotes = ''; // Biến lưu trữ nội dung của notes
+      for (int i = 0; i < userNotes.length; i++) {
+        final String userNote = userNotes[i];
+        if (userNote.startsWith(currentUserUID)) {
+          initialNotes = userNote.substring(currentUserUID.length + 1); // Lấy nội dung của notes
+          break;
+        }
+      }
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Take Notes'),
+          backgroundColor: Colors.orange,
+          content: Container(
+            height: MediaQuery.of(context).size.height * 0.7, // Thiết lập chiều cao của hộp thoại là 70% của chiều cao màn hình
+            padding: EdgeInsets.symmetric(vertical: 8.0), // Khoảng cách giữa nội dung và biên của hộp thoại
+            child: TextField(
+              controller: _notesController..text = initialNotes, // Hiển thị nội dung của notes (nếu có) trong TextField
+              expands: true, // Cho phép TextField mở rộng theo chiều dọc khi có nhiều nội dung hơn
+              minLines: null, // Số dòng tối thiểu hiển thị khi nhập
+              maxLines: null, // Số dòng tối đa hiển thị khi nhập, null để không giới hạn
+              textAlignVertical: TextAlignVertical.top, // Đặt vị trí bắt đầu nhập ở dòng trên cùng
+              decoration: InputDecoration(
+                hintText: 'Nhập nội dung notes',
+                border: OutlineInputBorder(), // Định dạng viền cho TextField
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Đóng hộp thoại
+              },
+              child: Text('Hủy',style: TextStyle(color: Colors.white),),
+
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _saveNotes(); // Lưu notes khi ấn nút Xác nhận
+                Navigator.pop(context); // Đóng hộp thoại
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white, // Đặt màu nền của nút là màu trắng
+              ),
+              child: Text(
+                'Xác nhận',
+                style: TextStyle(color: Colors.black), // Đặt màu chữ của nút là màu đen
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void _saveNotes() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final currentUserUID = user.uid;
+      final notes = _notesController.text;
+      final combinedNotes = '$currentUserUID-$notes'; // Kết hợp uid và notes
+
+      // Kiểm tra xem đã có notes của người dùng hiện tại chưa
+      final DocumentSnapshot document = await FirebaseFirestore.instance.collection('food').doc(widget.documentId).get();
+      final List<dynamic> userNotes = document['UserNotes'] ?? [];
+
+      // Tìm và cập nhật lại notes của người dùng nếu đã tồn tại
+      for (int i = 0; i < userNotes.length; i++) {
+        final String userNote = userNotes[i];
+        if (userNote.startsWith(currentUserUID)) {
+          userNotes[i] = combinedNotes; // Thay đổi nội dung của notes
+          await FirebaseFirestore.instance.collection('food').doc(widget.documentId).update({
+            'UserNotes': userNotes,
+          });
+          return; // Thoát khỏi hàm sau khi cập nhật notes thành công
+        }
+      }
+
+      // Nếu không tìm thấy notes của người dùng, thêm mới vào mảng UserNotes
+      await FirebaseFirestore.instance.collection('food').doc(widget.documentId).update({
+        'UserNotes': FieldValue.arrayUnion([combinedNotes]),
+      });
+    }
+  }
   List<String> splitHuongDan(String huongDan) {
     return huongDan.split('-').map((step) => step.trim()).toList();
   }
@@ -50,6 +150,14 @@ class _FoodPageDetailState extends State<FoodPageDetail> {
           "Công thức nấu ăn",
           style: TextStyle(color: Colors.white), // Màu chữ là màu trắng
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.note_add), // Icon cho chức năng take notes
+            onPressed: () {
+              _showNotesDialog();
+            },
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.orange,
